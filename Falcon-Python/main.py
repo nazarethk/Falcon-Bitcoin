4444f11e0dc775364fcf7cbfd5fdc27876148486
nazarethk <nazareth.keshishian@lau.edu>
2023-10-31 22:35:25 +0400

    Code side enhancements

--- Falcon-Python/main.py
+++ Falcon-Python/main.py
@@ -1,20 +1,33 @@
 import falcon
-from cryptos import Bitcoin , sha256
-c = Bitcoin(testnet=True)
+from cryptos import Bitcoin , sha256, ripemd160
+c = Bitcoin(testnet=False)
+
+def printCredentials(name, sk, pk, address):
+    print("###################")
+    print(name, "credentials:")
+    print("###################\n")
+    print("Secret Vector:",sk.h)
+    print("\nSecret Key:", sk.sha,"(",len(sk.sha)/2,"bytes)")
+    print("\nPublic Key (uncompressed):" ,pk.hex, "(",len(pk.hex)/2,"bytes)\n")
+    print("\nPublic Key Hash:" ,pk.sha, "(",len(pk.sha)/2,"bytes)\n")
+    validity = "is valid" if c.is_p2sh(address) else "is not valid"
+    print(name, "address:", address, validity)
+
 def generateKeys():
     sk = falcon.SecretKey(256)
     sk.sha = sha256(sk.hex)
     pk = falcon.PublicKey(sk)
-    pk.h = [int(x) for x in bytes.fromhex(pk.hex)]
-    address = c.pubtop2wpkh_p2sh(pk.h) # P2SH (BASE58) - Segwit - Pay to Script Hash
+    pk.sha = sha256(pk.hex)
+    pk.h = [int(x) for x in bytes.fromhex(pk.hex)] # vector 
+    address = c.pubtop2wpkh_p2sh(ripemd160(pk.sha)) # P2SH (BASE58) - Segwit - Pay to Script Hash
     return sk, pk ,address
 
 Alice_SecretKey, Alice_PublicKey, Alice_address = generateKeys()
-print("###################\nAlice credentials:\n###################\nSecret Vector:",Alice_SecretKey.h,"\n\nSecret Key:", Alice_SecretKey.sha, "(",len(Alice_SecretKey.sha)/2,"bytes)\n\nPublic Key (uncompressed):" ,Alice_PublicKey.hex,  "(",len(Alice_PublicKey.hex)/2,"bytes)\n\nAlice address:", Alice_address)
+printCredentials("Alice", Alice_SecretKey, Alice_PublicKey, Alice_address)
 
 Bob_SecretKey, Bob_PublicKey, Bob_address = generateKeys()
+printCredentials("Bob", Bob_SecretKey, Bob_PublicKey, Bob_address)
 
-print("\n###################\nBob credentials:\n###################\nSecret Vector:",Bob_SecretKey.h,"\n\nSecret Key:", Bob_SecretKey.sha, "(",len(Bob_SecretKey.sha)/2,"bytes)\n\nPublic Key (uncompressed):" ,Bob_PublicKey.hex,"(",len(Bob_PublicKey.hex)/2,"bytes)\n\nBob address:",Bob_address)
 message = "Bitcoin is great"
 sig = Alice_SecretKey.sign(message.encode())
 
